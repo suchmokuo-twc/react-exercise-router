29b036e2974c68124b7059cb60485e08b12742bf
SUCHMOKUO <suchmokuo@gmail.com>
2020-09-24 00:09:57 +0800

    [liuxing] finish exercise-4

--- src/exercise-4/components/App.js
+++ src/exercise-4/components/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Route} from "react-router";
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Route, Switch } from "react-router";
 import Home from "./Home";
 import About from "./About";
 import User from "./User";
@@ -11,10 +11,12 @@ class App extends Component {
     return (
       <div className="app">
         <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/:user(\d+)" component={User} />
+            <Route exact path="/about" component={About} />
             <Route component={NotMatch} />
-            <Route path='/' component={Home} />
-            <Route path='/:user' component={User} />
-            <Route path='/about' component={About} />
+          </Switch>
         </Router>
       </div>
     );
